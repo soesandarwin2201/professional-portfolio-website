01e8e08d255737efdd89747b76535a576dc4f9a1
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-06-01 10:24:48 +0630

    Modify the project card spelling error

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -19,11 +19,7 @@ const Projects = () => {
     variants={fadeIn("", "", 0.1, 1)}
     className='mt-3 text-slate-00 text-[17px] max-w-3xl leading-[30px]'
   >
-    Following projects showcases my skills and experience through
-    real-world examples of my work. Each project is briefly described with
-    links to code repositories and live demos in it. It reflects my
-    ability to solve complex problems, work with different technologies,
-    and manage projects effectively.
+    The following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos. They reflect my ability to solve complex problems, work with different technologies, and effectively manage projects.
   </motion.p>
   </div>
   
